40b9c61376fd839fdd294f86bac43da3862c46d4
DoLevi <domi.genz@gmail.com>
2019-11-10 15:42:17 +0100

    Introduced Main class

--- src/main/java/de/untenrechts/c4control/C4Control.java
+++ src/main/java/de/untenrechts/c4control/C4Control.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package de.untenrechts.c4control;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = C4Control.MODID, name = C4Control.NAME, version = C4Control.VERSION)
+public class C4Control
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "c4control";
+    public static final String NAME = "C for Control";
+    public static final String VERSION = "1.12.2-0.0.1";
 
     private static Logger logger;
 
